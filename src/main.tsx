c99587ebbbea9529e40792931af687465713d247
Quang Huy <quanghuy2003.hh@gmail.com>
2024-12-12 21:51:19 +0700

    npm run build

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { ThemeProvider } from "./contexts/ThemeContext.tsx";
+import { ThemeProvider } from "contexts/ThemeContext";
+import App from "App";
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <Provider store={store}>
