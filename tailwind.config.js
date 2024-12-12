6eefc1c2e881680e4e26239a849460f2d0349739
Quang Huy <quanghuy2003.hh@gmail.com>
2024-12-12 22:47:52 +0700

    build 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // Kích hoạt chế độ dark
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class", // Kích hoạt chế độ dark
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  
-}
+};
